aa62f85e7b92206687740d1e54389170dcd1356e
Meghanagoli <meghanagoli05@gmail.com>
2025-01-16 12:13:58 +0530

    Final Code

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const app = express();
 const PORT = 9999;
 const path = require('path');
 const PDFDocument = require('pdfkit');
-const fs = require('fs');
 const drSchema = require('./schemas/druser');
 const elderSchema = require("./schemas/elduser");
 const registerwardSchema=require("./schemas/registerward");
